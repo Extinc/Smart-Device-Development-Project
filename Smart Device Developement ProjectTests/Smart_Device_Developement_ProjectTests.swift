6504483bb7a30155feb95f8bac9f2d3cc5afd0ed
KY <limkeiyiang@lims-mac.local>
2018-05-15 22:39:13 +0800

    Test

---
+++ Smart Device Developement ProjectTests/Smart_Device_Developement_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Smart_Device_Developement_ProjectTests.swift
+//  Smart Device Developement ProjectTests
+//
+//  Created by ITP312 on 15/5/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import XCTest
+@testable import Smart_Device_Developement_Project
+
+class Smart_Device_Developement_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

3ca8a6967c3c1066d717ca3b2c1c65bfe0c67618
Aloysius <aloysius@aloysiuss-macbook-pro.local>
2018-05-19 17:53:22 +0800

    added Main UI

--- Smart Device Developement Project/ViewController/HRA/HRAViewController.swift
+++ Smart Device Developement Project/ViewController/HRA/HRAViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class HRAViewController: UIViewController {
-
+    
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +24,5 @@ class HRAViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

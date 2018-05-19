f3a44859bbf406e74caef4b2dd38045a92bc5c18
KY <limkeiyiang@lims-mac.local>
2018-05-19 15:39:55 +0800

    Updated Signup Page

--- Smart Device Developement Project/ViewController/Register/SignUpViewController.swift
+++ Smart Device Developement Project/ViewController/Register/SignUpViewController.swift
@@ -29,40 +29,18 @@ class SignUpViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        signupBtn.setTitle("Sign Up", for: UIControlState())
-        signupBtn.backgroundColor = MDCPalette.indigo.accent700
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-		
+        let buttonScheme = MDCButtonScheme()
+        MDCContainedButtonThemer.applyScheme(buttonScheme, to: signupBtn)
+        signupBtn.setTitle("Sign Up", for: UIControlState())
         setUpTextField()
     }
-
-    func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
-        }
-        
-        if ((cString.count) != 6) {
-            return UIColor.gray
-        }
-        
-        var rgbValue:UInt32 = 0
-        Scanner(string: cString).scanHexInt32(&rgbValue)
-        
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-
     
     func setUpTextField(){
             let reguserController = MDCTextInputControllerOutlined(textInput: reguser)

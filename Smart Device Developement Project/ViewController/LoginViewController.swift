1a4bbba1453f44f8b090a1cfe8c4fc01e0902976
KY <limkeiyiang@lims-mac.local>
2018-05-19 16:53:56 +0800

    Updated Login and Sign Up view

---
+++ Smart Device Developement Project/ViewController/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  Smart Device Developement Project
+//
+//  Created by lim kei yiang on 19/5/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginUser: MDCTextField! = {
+        let username = MDCTextField()
+        username.autocapitalizationType = .words
+        username.backgroundColor = .white
+        return username
+    }()
+    
+    @IBOutlet weak var loginPwd: MDCTextField! = {
+        let pwd = MDCTextField()
+        pwd.autocapitalizationType = .words
+        pwd.backgroundColor = .white
+        return pwd
+    }()
+    
+    var allTextFieldControllers = [MDCTextInputControllerFloatingPlaceholder]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupTextField()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func setupTextField(){
+        let loginUserController = MDCTextInputControllerOutlined(textInput: loginUser)
+        loginUserController.placeholderText = "Username"
+        allTextFieldControllers.append(loginUserController)
+        
+        let loginPwdController = MDCTextInputControllerOutlined(textInput: loginPwd)
+        loginPwdController.placeholderText = "Password"
+        allTextFieldControllers.append(loginPwdController)
+    }
+}
--- Smart Device Developement Project/ViewController/SignUpViewController.swift
+++ Smart Device Developement Project/ViewController/SignUpViewController.swift
@@ -27,6 +27,10 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var signupBtn: MDCFlatButton!
     var allTextFieldControllers = [MDCTextInputControllerFloatingPlaceholder]()
     
+    let primaryColor = UIColor(red: 0.19, green: 0.25, blue: 0.62, alpha: 1.0);
+    let primaryLightColor = UIColor(red: 0.40, green: 0.42, blue: 0.82, alpha: 1.0);
+    let primaryDarkColor = UIColor(red: 0.00, green: 0.10, blue: 0.44, alpha: 1.0);
+    let primaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -38,9 +42,16 @@ class SignUpViewController: UIViewController {
         // Do any additional setup after loading the view.
         let buttonScheme = MDCButtonScheme()
         MDCContainedButtonThemer.applyScheme(buttonScheme, to: signupBtn)
+        let colorScheme = MDCSemanticColorScheme()
+        let colors = Colors()
+        colorScheme.primaryColor = colors.primaryColor
+
+        MDCButtonColorThemer.applySemanticColorScheme(colorScheme, to: signupBtn)
         signupBtn.setTitle("Sign Up", for: UIControlState())
         setUpTextField()
     }
+    
+    
     
     func setUpTextField(){
             let reguserController = MDCTextInputControllerOutlined(textInput: reguser)

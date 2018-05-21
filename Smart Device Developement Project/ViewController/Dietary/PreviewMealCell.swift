b58ecf41f96e061cee9da4d6449e90c360358212
Guan Wei <guanwei@guans-imac.local>
2018-05-21 21:18:34 +0800

    updated table view stuff

---
+++ Smart Device Developement Project/ViewController/Dietary/PreviewMealCell.swift
@@ -0,0 +1,24 @@
+//
+//  PreviewMealCell.swift
+//  Smart Device Developement Project
+//
+//  Created by Guan Wei on 21/5/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class PreviewMealCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

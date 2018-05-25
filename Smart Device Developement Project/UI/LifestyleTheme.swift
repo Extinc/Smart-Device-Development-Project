f8d56dfe96c8ef87b44b799dae0257dc82b6f64b
FKY <alphatoon@hotmail.com>
2018-05-25 08:28:57 +0800

    Updated & Added icon for 3 storyboard
    Not including Dietary

---
+++ Smart Device Developement Project/UI/LifestyleTheme.swift
@@ -0,0 +1,23 @@
+//
+//  LifestyleTheme.swift
+//  Smart Device Developement Project
+//
+//  Created by ITP312 on 25/5/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents
+
+class LifestyleTheme: NSObject {
+    let colors:Colors
+    
+    override init() {
+        colors = Colors()
+    }
+    
+    func styleBtn(btn: MDCFlatButton, title: String){
+        
+    }
+    
+}

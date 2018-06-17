77bdfcaaba08e43801ca15b71dac6a1c226d99b1
KY <alphatoon@Hotmail.com>
2018-06-17 21:07:47 +0800

    Added BridgingHeader for Sqlitedb

---
+++ Smart Device Developement Project/Helpers/BridgingHeader.m
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.m
+//  Smart Device Developement Project
+//
+//  Created by lim kei yiang on 17/6/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+#import "BridgingHeader.h"
+
+@implementation BridgingHeader
+
+@end

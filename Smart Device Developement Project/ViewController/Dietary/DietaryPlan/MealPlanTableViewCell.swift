64b040756b3d5cf3da0f82cffa1042fdbb9a1f45
Guan Wei <guanwei@guans-imac.local>
2018-06-20 23:03:32 +0800

    deleted views

---
+++ Smart Device Developement Project/ViewController/Dietary/DietaryPlan/MealPlanTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealPlanTableViewCell.swift
+//  Smart Device Developement Project
+//
+//  Created by Guan Wei on 20/6/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class MealPlanTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

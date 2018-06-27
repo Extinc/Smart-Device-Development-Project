b25e889972fbf7d2efd7b44622bd5f08bd539f22
Aloysius <165375k@mymail.nyp.edu.sg>
2018-06-27 19:00:46 +0800

    FoodAPI v1
    havent do cocoa pods install

---
+++ Smart Device Developement Project/DataManager/Dietary/FoodAPI.h
@@ -0,0 +1,19 @@
+//
+//  FoodAPI.h
+//  Smart Device Developement Project
+//
+//  Created by ITP312 on 27/6/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+#ifndef FoodAPI_h
+#define FoodAPI_h
+
+@interface FoodAPI : NSObject
+
+
++ (BOOL)downloadAPI:(NSString *)food;
+
+
+@end
+#endif /* FoodAPI_h */

4b89a263f7bca42df821fbabbe768c9c08a0fa69
FKY <alphatoon@hotmail.com>
2018-07-17 10:42:51 +0800

    for charts reducedEntries change to flatMap instead of Compact and for timer label added to bridging header

---
+++ Smart Device Developement Project/ViewController/Workout/WorkoutStartViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WorkoutStartViewController.swift
+//  Smart Device Developement Project
+//
+//  Created by ITP312 on 17/7/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import UIKit
+import MZTimerLabel
+
+class WorkoutStartViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: MZTimerLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.'
+        timerLabel.start()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

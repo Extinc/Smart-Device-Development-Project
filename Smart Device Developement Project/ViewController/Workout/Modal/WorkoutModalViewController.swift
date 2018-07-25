8b231c589bd52abee845c44cdbc5fbf602a370c4
FKY <alphatoon@hotmail.com>
2018-07-25 12:39:56 +0800

    Update

--- Smart Device Developement Project/ViewController/Workout/Modal/WorkoutModalViewController.swift
+++ Smart Device Developement Project/ViewController/Workout/Modal/WorkoutModalViewController.swift
@@ -16,7 +16,9 @@ class WorkoutModalViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.view.isOpaque = false
+        self.showAnimate()
         LifestyleTheme.styleFloatBtn(btn: addBtn, title: "", pColor: Colors.PrimaryColor())
     }
 
@@ -25,6 +27,29 @@ class WorkoutModalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.8;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 0.8
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+        
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
 
     /*
     // MARK: - Navigation
